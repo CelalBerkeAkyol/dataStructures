b55d93e7d129c81ac632644693aa2980280f67d7
CelalBerkeAkyol <152324363+CelalBerkeAkyol@users.noreply.github.com>
2023-12-06 18:18:28 +0300

    Update Stack_Arrays.c

--- Stack_Arrays.c
+++ Stack_Arrays.c
@@ -1,55 +1,105 @@
 #include <stdio.h>
 #include <stdlib.h>
-// genel tanımlılar
-int arr[20];
-int top = -1;
-// fonksiyonlar
-void push(int data)
-{
-    top++;
-    arr[top] = data;
-}
-void pop()
-{
-    top--;
-}
-void listele()
-{
-    for (int i = top; i >=0; i--)
-    {
-        printf("%d\n", arr[i]);
-    }
-}
+#include <stdbool.h>
+#define MAX 5
+#define TOP_EMPTY -1
+int top = TOP_EMPTY;
+int stack_array[MAX];
+int data; // silinen değeri saklayan değişken
+// fonksiyon tanımları
+bool isFull();
+void push();
+int pop();
+void peek();
+void show();
+
 int main()
 {
+    int choice;
 
     while (1)
     {
-        printf("Hangi işlemi yapmak istersiniz ? \n {1} eleman eklemek \n {2} eleman silmek \n {3} listeyi görüntülemek\n {4} programı sonlandırmak");
-        int secim;
-        scanf("%d", &secim);
-        switch (secim)
+        printf("\n***Hangi işlemi yapmak istersiniz ? ***\n");
+        printf(" {0} programdan çıkmak \n {1} eleman eklemek \n {2} eleman silmek \n {3} en son elemanın değeri? \n {4} stack dolu mu ? \n {5} Tüm elemanları göster\n ");
+        scanf("%d", &choice);
+        switch (choice)
         {
+        case 0:
+            exit(EXIT_SUCCESS);
+            break;
         case 1:
-            printf("Hangi değeri girmek isteersiniz");
-            int deger; 
-            scanf("%d",&deger);
-            push(deger);
+            push();
             break;
         case 2:
-            pop();
+            data = pop();
             break;
         case 3:
-            listele();
+            peek();
             break;
         case 4:
-            exit(EXIT_SUCCESS); 
-                        break;
+            isFull();
+            break;
+        case 5:
+            show();
+            break;
 
         default:
-            printf("Böyle bir seçenek bulunmamaktadır. ") ;
+            printf("Lütfen doğru değer giriniz");
             break;
         }
     }
-    return 0;
+}
+bool isFull()
+{
+    if (top == MAX - 1)
+    {
+        printf("Stack dolu");
+        return true;
+    }
+    else{
+        printf("Stack boş");
+                return false;
+
+    }
+}
+void push()
+{
+    if (isFull)
+    {
+        printf("Stack overflow \n");
+        return;
+    }
+    else
+    {
+        top++;
+        printf("Hangi değeri girmek istersiniz \t:");
+        scanf("%d",&data);
+        stack_array[top] = data;
+        printf("Eleman eklendi = %d",data);
+    }
+}
+int pop()
+{
+    if (top == TOP_EMPTY)
+    {
+        printf("Stack underflow");
+        exit(EXIT_FAILURE);
+    }
+    else
+    {
+        data = stack_array[top];
+        top--;
+        return data; // silinen data
+        printf("Eleman silindi = %d",data);
+    }
+}
+void peek(){
+    printf("En son elemanın değeri = %d \n",stack_array[top]);
+}
+void show(){
+    for (int i = top; i>= 0; i--)
+    {
+        printf("%d\n",stack_array[i]);
+    }
+    
 }
